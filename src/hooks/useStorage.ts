907a30c92df94448356cf9433c50ba89bc43ae1d
皓月 <zhangwenhaoyue@163.com>
2024-11-26 23:39:52 +0800

    feat: 新增un请求库

---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,86 @@
+
+export function useStorage() {
+    const suffix = '_deadtime'
+
+    /**
+     * 获取
+     * @param {*} key 关键字
+     */
+    function get(key: string): any {
+        return uni.getStorageSync(key);
+    }
+
+    /**
+    * 获取全部
+    */
+    function info() {
+        const { keys } = uni.getStorageInfoSync();
+        const data: any = {};
+
+        keys.forEach((e: string) => {
+            data[e] = uni.getStorageSync(e);
+        });
+
+        return data;
+    }
+
+    /**
+     * 设置
+     * @param {*} key 关键字
+     * @param {*} value 值
+     * @param {*} expires 过期时间
+     */
+    function set(key: string, value: any, expires?: number): void {
+        uni.setStorageSync(key, value);
+
+        if (expires) {
+            uni.setStorageSync(
+                `${key}${suffix}`,
+                Date.parse(String(new Date())) + expires * 1000
+            );
+        }
+    }
+
+    /**
+     * 是否过期
+     * @param {*} key 关键字
+     */
+    function isExpired(key: string): boolean {
+        return uni.getStorageSync(`${key}${suffix}`) - Date.parse(String(new Date())) <= 0;
+    }
+
+    /**
+     * 删除
+     * @param {*} key 关键字
+     */
+    function remove(key: string) {
+        return uni.removeStorageSync(key);
+    }
+
+    /**
+     * 清理
+     */
+    function clear() {
+        uni.clearStorageSync();
+    }
+
+    /**
+     * 获取一次后删除
+     */
+    function once(key: string) {
+        const value = get(key);
+        remove(key);
+        return value;
+    }
+
+    return {
+        get,
+        info,
+        set,
+        isExpired,
+        remove,
+        clear,
+        once,
+        suffix,
+    }
+}
