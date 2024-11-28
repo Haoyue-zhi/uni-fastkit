891b42f0c2c80c483953d020478ce8a245492621
皓月 <zhangwenhaoyue@163.com>
2024-11-28 23:05:26 +0800

    chore: 修改stylelint配置

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: [
+		'stylelint-config-recommended-vue',
+		'stylelint-config-recommended-vue/scss',
+		'stylelint-config-recess-order',
+	]
+}
