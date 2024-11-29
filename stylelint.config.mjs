b3d23e213ae0da3beb3beee2cf0cf1d94949a9b7
皓月 <zhangwenhaoyue@163.com>
2024-11-29 12:07:41 +0800

    chore: 修改stylelint配置

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,8 +1,34 @@
 /** @type {import('stylelint').Config} */
 export default {
-	extends: [
-		'stylelint-config-recommended-vue',
-		'stylelint-config-recommended-vue/scss',
-		'stylelint-config-recess-order',
-	]
+  extends: [
+    'stylelint-config-recommended-vue',
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-recess-order',
+  ],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['deep', 'slotted', 'global'],
+      },
+    ],
+    'unit-no-unknown': [
+      true,
+      {
+        ignoreUnits: ['rpx'],
+      },
+    ],
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignoreTypes: ['page', 'rich-text', 'scroll-view'],
+      },
+    ],
+    'comment-empty-line-before': 'never',
+    'custom-property-empty-line-before': 'always',
+    'no-empty-source': null,
+    'no-duplicate-selectors': null,
+    'selector-class-pattern': null,
+    'font-family-no-missing-generic-family-keyword': null,
+  },
 }
