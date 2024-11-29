07a76cd8ae3a5748a2a7bb98f76e3b9970aafe80
皓月 <zhangwenhaoyue@163.com>
2024-11-29 12:06:43 +0800

    chore: 修改eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,26 +3,26 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 import tailwind from 'eslint-plugin-tailwindcss'
 
 export default uniHelper(
-	{
-		uni: true,
-		uniJson: true,
-		vue: true,
-		jsx: false,
-		stylistic: false,
-		typescript: {
-			parserOptions: {
-				extraFileExtensions: ['.vue', '.nvue'],
-			},
-		},
-		ignores: ['**/*.d.ts', 'vite.config.ts'],
-	},
-	eslintConfigPrettier,
-	...tailwind.configs['flat/recommended'],
-	{
-		settings: {
-			tailwindcss: {
-				whitelist: ['cus-[A-Za-z].*', '.*-uni'],
-			},
-		},
-	}
+  {
+    uni: true,
+    uniJson: true,
+    vue: true,
+    jsx: false,
+    stylistic: false,
+    typescript: {
+      parserOptions: {
+        extraFileExtensions: ['.vue', '.nvue'],
+      },
+    },
+    ignores: ['**/*.d.ts', 'vite.config.ts', 'src/uni_modules/', 'src/nativeplugins/'],
+  },
+  eslintConfigPrettier,
+  ...tailwind.configs['flat/recommended'],
+  {
+    settings: {
+      tailwindcss: {
+        whitelist: ['cus-[A-Za-z].*', '.*-uni'],
+      },
+    },
+  },
 )
