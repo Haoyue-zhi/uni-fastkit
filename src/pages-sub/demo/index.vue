684fb2622fcf993fdf86af5222672cf1ad27093c
皓月 <zhangwenhaoyue@163.com>
2024-12-02 15:10:21 +0800

    chore: 新增分包页面

---
+++ src/pages-sub/demo/index.vue
@@ -0,0 +1,14 @@
+<route lang="json5" type="page">
+{
+  style: { navigationBarTitleText: '分包页面 标题' },
+}
+</route>
+
+<script setup lang="ts"></script>
+
+<template>
+  <view class="text-center">
+    <view class="m-8">http://localhost:666/#/pages-sub/demo/index</view>
+    <view class="text-green-500">分包页面demo</view>
+  </view>
+</template>
