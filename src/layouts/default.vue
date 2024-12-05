6ffd7903266a5b6523986766531f7540645ffd08
皓月 <zhangwenhaoyue@163.com>
2024-12-05 17:14:50 +0800

    feat: 新增page-meta标签

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+const scrollState = ref(false)
+
+function lockScroll(val: boolean) {
+  scrollState.value = val
+}
+
 const themeVars = reactive({
   // primaryColor: '#008000',
   // primaryColorEnd: '#008000',
 })
+
+function setTheme(val: Record<string, string>) {
+  Object.assign(themeVars, val)
+}
+
+defineExpose({
+  setTheme,
+  lockScroll,
+})
 </script>
 
 <template>
+  <!-- #ifdef APP -->
+  <page-meta :page-style="`overflow:${scrollState ? 'hidden' : 'visible'};`" />
+  <!-- #endif -->
   <nut-config-provider :theme-vars="themeVars">
     <slot />
     <nut-toast></nut-toast>
