521d73a5a74d22d56efffd5e4d292d91c9599647
皓月 <zhangwenhaoyue@163.com>
2024-12-05 20:55:30 +0800

    chore: 新增对WebStorm支持

---
+++ .idea/fileTemplates/v3.vue
@@ -0,0 +1,15 @@
+<route lang="json5" type="home">
+{
+  style: {
+    navigationBarTitleText: '',
+  },
+}
+</route>
+
+<script setup lang="ts"></script>
+
+<template>
+  <view></view>
+</template>
+
+<style scoped lang="scss"></style>
