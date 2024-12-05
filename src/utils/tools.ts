595cbae46b8774a158c15552a67f06885e48213d
皓月 <zhangwenhaoyue@163.com>
2024-12-05 21:45:23 +0800

    feat: 新增onAlipaySandbox开启支付宝沙箱

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -31,3 +31,35 @@ export function restoreUrl(path: string, query: Record<string, unknown>) {
   }
   return path
 }
+
+/**
+ * 开启或关闭支付宝沙箱环境
+ *
+ * @param on - 是否开启沙箱环境，默认为 true
+ * @throws {Error} 如果 plus 未定义或出现其他异常
+ */
+export function onAlipaySandbox(on: boolean = true) {
+  if (!on) {
+    // eslint-disable-next-line no-console
+    console.log(
+      '%c支付宝沙箱已关闭',
+      'background:#1677ff;color:#fff;padding:2px 4px;border-radius:4px;',
+    )
+    return
+  }
+
+  try {
+    if (!plus) return
+    const EnvUtils = plus.android.importClass('com.alipay.sdk.app.EnvUtils')
+    // eslint-disable-next-line ts/ban-ts-comment
+    // @ts-expect-error
+    EnvUtils.setEnv(EnvUtils.EnvEnum.SANDBOX)
+    // eslint-disable-next-line no-console
+    console.log(
+      '%c支付宝沙箱已开启',
+      'background:#1677ff;color:#fff;padding:2px 4px;border-radius:4px;',
+    )
+  } catch (e) {
+    console.error('支付宝沙箱开启失败', e)
+  }
+}
