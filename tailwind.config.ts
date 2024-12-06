ea1fad9ff8189b54bd0505ce12d90dea69594e8c
皓月 <zhangwenhaoyue@163.com>
2024-12-06 19:06:33 +0800

    chore: 移除tailwindcss对nvue的支持

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 import { addDynamicIconSelectors } from '@iconify/tailwind'
 
 export default {
-  content: ['./index.html', './src/**/*.{vue,nvue,js,ts}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
     extend: {
       height: {
