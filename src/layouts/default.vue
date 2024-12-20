9a777cd544c61c6aa9eb563e84a361307a1dddbf
皓月 <zhangwenhaoyue@163.com>
2024-12-20 10:05:07 +0800

    feat: 新增 默认布局添加header、footer插槽，添加通用主题文件

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import emitter from '@/service/helper'
+import { addUnit, getRect } from '@/utils/tools'
+import { configProviderThemeVars } from './theme'
 
 const toast = useToast('serviceToast')
 
@@ -14,6 +16,7 @@ function lockScroll(val: boolean) {
 }
 
 const themeVars = reactive({
+  ...configProviderThemeVars,
   // primaryColor: '#008000',
   // primaryColorEnd: '#008000',
 })
@@ -21,6 +24,24 @@ const themeVars = reactive({
 function setTheme(val: Record<string, string>) {
   Object.assign(themeVars, val)
 }
+
+const header = ref()
+const footer = ref()
+const headerHeight = ref(0)
+const footerHeight = ref(0)
+
+onMounted(() => {
+  if (header.value) {
+    getRect('#header', false).then((res) => {
+      headerHeight.value = Number(res.height)
+    })
+  }
+  if (footer.value) {
+    getRect('#footer', false).then((res) => {
+      footerHeight.value = Number(res.height)
+    })
+  }
+})
 
 defineExpose({
   setTheme,
@@ -33,7 +54,21 @@ defineExpose({
   <page-meta :page-style="`overflow:${scrollState ? 'hidden' : 'visible'};`" />
   <!-- #endif -->
   <nut-config-provider :theme-vars="themeVars">
+    <view v-if="$slots.header" id="header" ref="header" class="sticky top-[var(--window-top)] z-10">
+      <slot name="header" />
+    </view>
+    <!-- #ifndef MP -->
+    <slot :footer-height="footerHeight" :header-height="headerHeight" />
+    <!-- #endif -->
+    <!-- #ifdef MP -->
     <slot />
+    <!-- #endif -->
+    <view v-if="$slots.footer" :style="{ height: addUnit(footerHeight) }">
+      <view id="footer" ref="footer" class="fixed bottom-[var(--window-bottom)] z-[1] w-full">
+        <slot name="footer" />
+      </view>
+    </view>
+
     <nut-toast />
     <nut-notify />
 
