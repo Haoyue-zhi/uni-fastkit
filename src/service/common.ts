6406696ca134da48cca8a8805d66d0dec834c485
皓月 <zhangwenhaoyue@163.com>
2024-12-25 09:25:34 +0800

    chore: 移除content-type

--- src/service/common.ts
+++ src/service/common.ts
@@ -3,7 +3,6 @@ import pkg from '@/manifest.json'
 export const DefaultBaseUrl = import.meta.env.VITE_BASE_URL
 export const DefaultHeaders = {
   Accept: 'application/json',
-  'Content-Type': 'application/json; charset=utf-8',
   'X-Version': `${encodeURIComponent(pkg.name)}/${pkg.versionCode}`,
 }
 export const timeout = 1000 * 3
