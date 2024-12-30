e52dd7cde44eea731a585df3fe6d848c591218d7
皓月 <zhangwenhaoyue@163.com>
2024-12-30 14:12:55 +0800

    chore: 优化

--- src/hooks/useErrorToast.ts
+++ src/hooks/useErrorToast.ts
@@ -1,34 +1,38 @@
 import type { ToastInst } from 'nutui-uniapp'
 import emitter from '@/service/helper'
 export function useErrorToast(toast?: ToastInst) {
-  let fn: (msg: string) => void
+  let errorFn: (msg: string) => void
   function useToast(msg: string) {
-    return (toast?: ToastInst) => {
-      toast ? toast.error(msg) : uni.showToast({ title: msg, icon: 'none' })
-    }
+    return toast ? toast.error(msg) : uni.showToast({ title: msg, icon: 'none' })
   }
 
-  function addErrorToast(toast?: ToastInst) {
-    fn = (msg: string) => useToast(msg)(toast)
-    emitter.on('API_ERROR', fn)
+  function closeToast() {
+    toast ? toast.hide() : uni.hideToast()
+  }
+
+  function addErrorToast() {
+    errorFn = (msg: string) => useToast(msg)
+    emitter.on('API_ERROR', errorFn)
   }
 
   function delErrorToast() {
-    emitter.off('API_ERROR', fn)
+    emitter.off('API_ERROR', errorFn)
   }
 
   // 所有页面使用
   onShow(() => {
-    addErrorToast(toast)
+    addErrorToast()
   })
 
   // tabBar页使用
   onHide(() => {
     delErrorToast()
+    closeToast()
   })
 
   // 子页面使用
   onUnload(() => {
     delErrorToast()
+    closeToast()
   })
 }
