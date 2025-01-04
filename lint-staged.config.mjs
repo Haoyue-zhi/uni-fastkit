b4eb5918a0445e4abc349149eb4e7fcba81d6db1
皓月 <zhangwenhaoyue@163.com>
2025-01-04 22:29:15 +0800

    chore: 正式开源

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
   '*.{js,cjs,mjs,ts,cts,mts,vue,nvue}': 'eslint --fix',
-  '*.{html,js,cjs,mjs,ts,cts,mts,vue,nvue}': 'prettier --write',
-  '*.{scss,css,vue,nvue}': 'stylelint --fix',
+  '*.{html,js,cjs,mjs,ts,cts,mts,vue,nvue}': 'prettier --ignore-unknown --write',
+  '*.{scss,css,vue,nvue}': 'stylelint --fix --allow-empty-input',
 }
