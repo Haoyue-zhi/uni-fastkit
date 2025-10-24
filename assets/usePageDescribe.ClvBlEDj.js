77636e2536ffd2a3ce43a51a5e263f08d8b1d1dc
Haoyue-zhi <Haoyue-zhi@users.noreply.github.com>
2025-10-24 10:28:37 +0000

    Deploy H5 build: eac7543bdabe9657ebf8996b0e435600642de6b9 eac7543bdabe9657ebf8996b0e435600642de6b9

---
+++ assets/usePageDescribe.ClvBlEDj.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index-B2scKoCu.js";function a(a,o){e(()=>{a.value&&"function"==typeof a.value.setHeader?a.value.setHeader(o):console.warn("uniLayout实例不存在或没有setHeader方法")})}export{a as u};
